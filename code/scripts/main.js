b1fa5bf2d943131be879b83326331852cb978d39
lisa-pug <gregorylisa405@gmail.com>
2026-09-23 10:34:14 +0100

    updated main.js so that the html stuff works

--- code/scripts/main.js
+++ code/scripts/main.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Working');
-});
+// Serve everything in code/public/ (HTML, CSS, JS, images, etc.)
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
