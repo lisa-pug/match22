98b1b5030abbf4ae0c97606d36750b9b6c71d5ce
lisa-pug <gregorylisa405@gmail.com>
2026-09-25 13:58:37 +0100

    sign up funtionality & ui

---
+++ code/public/js/accounts.js
@@ -0,0 +1,85 @@
+function showPword() {
+  var x = document.getElementById("password-input");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function showDOBhelp() {
+  document.getElementById('signUpHelpBox').style.display = 'inline';
+}
+
+function hideDOBhelp() {
+  document.getElementById('signUpHelpBox').style.display = 'none';
+}
+
+async function signUp() {
+  const username = document.getElementById('username-input').value || null;
+  const password = document.getElementById('password-input').value || null;
+  const dob = document.getElementById('dob').value || null;
+  const birthYear = new Date(dob).getFullYear();
+  const thisYear = new Date().getFullYear();
+  // validation
+  // note that checking whether not not username exists in users table happens in the backend in main.js
+  if (username === null || password === null) {
+    alert("Please fill in both username and password.");
+    return;
+  } else if (username.length > 24 || username.length < 6) {
+    alert("Username must be 6-24 characters long (inclusive).");
+    return;
+  } else if (!validatePassword(password)) {
+    alert("Make sure your password obeys the bullet points below.");
+    return;
+  } else if (birthYear > thisYear) {
+    alert("Date of birth can't be in the future.")
+    return;
+  }
+  // initiate loading
+  const btn = document.getElementById('signUpButton');
+  const originalText = btn.innerHTML;
+  btn.disabled = true;
+  btn.innerHTML = '<span class="loader"></span>';
+
+  // sign up
+  const res = await fetch('/api/signup', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password, dob })
+  });
+  const data = await res.json();
+  console.log(data);
+  // stop loading
+  btn.disabled = false;
+  btn.innerHTML = originalText;
+  if (data.error) {
+    alert(data.error);
+  } else {
+    document.getElementById('successMsg').style.display = 'block';
+    // AUTO LOG IN!!!!!
+    // REDIRECT!!!! window.location.href = 'quiz.html'
+  }
+}
+
+function validatePassword(password) {
+  let upper = false;
+  let lower = false;
+  let num = false;
+  let spec = false;
+  for (const character of password) {
+    if (character >= 'A' && character <= 'Z') {
+      upper = true;
+    } else if (character >= 'a' && character <= 'z') {
+      lower = true;
+    } else if (character >= '0' && character <= '9') {
+      num = true;
+    } else {
+      spec = true;
+    }
+    if (upper && lower && num && spec) {
+      break;
+    }
+  }
+  return upper && lower && num && spec;
+}
--- code/scripts/main.js
+++ code/scripts/main.js
@@ -1,12 +1,49 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const pool = require('./db');
+const bcrypt = require('bcrypt');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json()); 
+
 // Serve everything in code/public/ (HTML, CSS, JS, images, etc.)
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/api/books', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM books');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database error' });
+  }
+});
+
+// sign up stuff
+app.post('/api/signup', async (req, res) => {
+  const { username, password, dob } = req.body;
+
+  try {
+    // check username doesn't already exist
+    const existing = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+    if (existing.rows.length > 0) {
+      return res.status(400).json({ error: 'Username taken or account already exists' });
+    }
+    // create account
+    const password_hash = await bcrypt.hash(password, 10);
+    const result = await pool.query(
+      'INSERT INTO users (username, password_hash, dob) VALUES ($1, $2, $3) RETURNING userid, username',
+      [username, password_hash, dob]
+    );
+    res.json(result.rows[0]);
+    // NEED TO AUTHENTICATE USER
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Signup failed' });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
